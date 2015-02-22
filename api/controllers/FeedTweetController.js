e7fa5e90720443fd7768842d7127430f0bf57bc6
unknown <pablophlam@gmail.com>
2015-02-21 20:33:28 -0500

    mongoDB connection, feedTweet controller

---
+++ api/controllers/FeedTweetController.js
@@ -0,0 +1,31 @@
+/**
+ * FeedTweetController
+ *
+ * @description :: Server-side logic for managing feedtweets
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `FeedTweetController.dbPush()`
+   */
+  dbPush: function (req, res) {
+    return res.json({
+      todo: 'dbPush() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `FeedTweetController.dbPull()`
+   */
+  dbPull: function (req, res) {
+    return res.json({
+      todo: 'dbPull() is not implemented yet!'
+    });
+  }
+};
+
