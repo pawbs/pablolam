660aa0d9a99cec717edfaf677a1ba1e90f362b67
unknown <pablophlam@gmail.com>
2015-04-04 05:52:39 -0400

    front-end work

--- assets/js/pablolam.js
+++ assets/js/pablolam.js
@@ -1,8 +1,18 @@
-angular.module('pablolam', ['ngResource', 'infinite-scroll'])
+angular.module('pablolam', ['ngResource', 'infinite-scroll', 'ngAnimate', 'duScroll'])
 
-.controller('FeedController', ['$scope', '$resource', function($scope, $resource) {
+.controller('FeedController', ['$scope', '$resource', '$timeout', '$templateCache', '$document',
+  function($scope, $resource, $timeout, $templateCache, $document) {
 
+  $scope.infiniteDisabled = true;
+  $scope.infiniteBusy = false;
+
+  $scope.showTwit = true;
+  $scope.showInstagram = true;
+  $scope.showScrobble = true;
+  
   var Json = $resource('/feed/dbPull');
+  
+  //QUERY FUNCTIONS
   var json = Json.query(function() {
   
     $scope.pabloFeed = []
@@ -13,17 +23,61 @@ angular.module('pablolam', ['ngResource', 'infinite-scroll'])
     $scope.pabloFeed.push(json[$scope.pabloFeed.length])
     $scope.pabloFeed.push(json[$scope.pabloFeed.length])
     $scope.pabloFeed.push(json[$scope.pabloFeed.length])
-  
+
     $scope.loadMore = function() {
-      $scope.pabloFeed.push(json[$scope.pabloFeed.length])
+      
+      if ($scope.infiniteBusy == false){
+      
+      $scope.infiniteBusy = true;
+      console.log("stop!")
+      
+        $timeout(function(){
+          $scope.infiniteBusy = false;
+          console.log("go!")
+          $scope.infiniteDisabled = false;
+          $scope.pabloFeed.push(json[$scope.pabloFeed.length])
+          $scope.pabloFeed.push(json[$scope.pabloFeed.length])
+          $scope.pabloFeed.push(json[$scope.pabloFeed.length])
+          $scope.pabloFeed.push(json[$scope.pabloFeed.length])
+          $scope.pabloFeed.push(json[$scope.pabloFeed.length])
+        },1000).then( function() {})
+      }
     }
   
   })
   
-
+  //FILTER FUNCTIONS
+  $scope.filterByType = function(block){
+    switch (block.type) {
+      case "twitter":
+        if ($scope.showTwit) {return block}
+        break
+      case "instagram":
+        if ($scope.showInstagram) {return block}
+        break
+      case "lastfm":
+        if ($scope.showScrobble) {return block}
+        break
+    }
+  }
   
+  $scope.toggleTwit = function(){
+    $scope.showTwit = !$scope.showTwit
+  }
+  $scope.toggleInstagram = function(){
+    $scope.showInstagram = !$scope.showInstagram
+  }
+  $scope.toggleScrobble = function(){
+    $scope.showScrobble = !$scope.showScrobble
+  }
   
-
+  //SCROLL FUNCTIONS
+  function easeInQuad(t) {
+    return 1+(--t)*t*t*t*t;
+  }
   
+  $scope.goToTop = function() {
+    $document.scrollTopAnimated(0, 500, easeInQuad).then(function() {});
+  }
   
 }]);
